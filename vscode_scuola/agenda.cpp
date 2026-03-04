8419aaad5fd3493c3669a4289c29547a499a9f0f
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-03-05 00:20:08 +0100

    Add main menu and functions to agenda.cpp
    I've continued my agenda project for school today: I didn't finish it,
    but I've finished the main functions (even if briefly, no code security
    whatsoever), and I deletend the security checks because of time.
    
    Still, those are fixable if I give it like 2 hours one day.

--- vscode_scuola/agenda.cpp
+++ vscode_scuola/agenda.cpp
@@ -12,111 +12,16 @@ struct Contatto
     int ncivico;
 };
 
-// FUNZIONI INTERNE A FUNZIONE OVERLOAD
-
-// FUNIONI NOME / COGNOME
-
-bool controlloNome(std::string &stringa)
-{
-    for (auto &c : stringa)
-    {
-        if (c > '0' && c < '9')
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool cfinaleNome(std::string &stringa)
-{
-    char scelta;
-    for (auto &c : stringa)
-    {
-        c = tolower(c);
-    }
-    stringa[0] = toupper(stringa[0]);
-    while (true)
-    {
-        std::cout << "Sei sicuro di questa scelta: " << stringa << " (y/n)? ";
-        std::cin >> std::ws >> scelta;
-        scelta = tolower(scelta);
-        switch (scelta)
-        {
-        case 'y':
-            return true;
-        case 'n':
-            return false;
-        default:
-            std::cout << "\nErrore: Scelta Non Valida.\n";
-        }
-    }
-}
-
-// FUNZIONI ETA
-
-bool controlloEta(int &eta)
-{
-    if (eta < 0 || eta > 150)
-    {
-        std::cout << "\nErrore: Età non valida.\n";
-        return false;
-    }
-    return true;
-}
-
-// FUNZIONE OVERLOAD
+// FUNZIONI OVERLOAD
 
 std::istream &operator>>(std::istream &in, Contatto &c)
 {
-    bool cnomi = false;
-    bool cnomif = false;
-    bool ceta = false;
-    while (!cnomi || !cnomif)
-    {
-        std::cout << "\nInserisci il nome del contatto: ";
-        std::getline(in >> std::ws, c.nome);
-        cnomi = controlloNome(c.nome);
-        if (!cnomi)
-        {
-            std::cout << "\nErrore: Nome Invalido.\n";
-        }
-        else
-        {
-            cnomif = cfinaleNome(c.nome);
-        }
-    }
-    cnomi = false;
-    cnomif = false;
-    while (!cnomi || !cnomif)
-    {
-        std::cout << "\nInserisci il cognome del contatto: ";
-        std::getline(in >> std::ws, c.cognome);
-        cnomi = controlloNome(c.cognome);
-        if (!cnomi)
-        {
-            std::cout << "\nErrore: Cognome Invalido.\n";
-        }
-        else
-        {
-            cnomif = cfinaleNome(c.cognome);
-        }
-    }
-    while (!ceta)
-    {
-        std::cout << "\nInserisci l'età del contatto: ";
-        in >> c.eta;
-        if (!(in >> c.eta))
-        {
-            ceta = false;
-            std::cout << "\nUn numero non lettere\n";
-            in.clear();
-            break;
-        }
-        else{
-            ceta = controlloEta(c.eta);
-        } 
-    }
+    std::cout << "\nInserisci il nome del contatto: ";
+    std::getline(in >> std::ws, c.nome);
+    std::cout << "\nInserisci il cognome del contatto: ";
+    std::getline(in >> std::ws, c.cognome);
+    std::cout << "\nInserisci l'età del contatto: ";
+    in >> c.eta;
     std::cout << "\nInserisci il numero di telefono del contatto: ";
     std::getline(in >> std::ws, c.ntelefono);
     std::cout << "\nInserisci l'indirizzo del contatto: ";
@@ -126,21 +31,145 @@ std::istream &operator>>(std::istream &in, Contatto &c)
     return in;
 }
 
-int main()
+std::ostream &operator<<(std::ostream &out, Contatto &c)
 {
-    std::vector<Contatto> agenda;
+    out << "\n\n\n"
+        << "---------- CONTATTO ---------\n\n";
+    out << "NOME E COGNOME: " << c.nome << " " << c.cognome;
+    out << "\nETÀ: " << c.eta;
+    out << "\nNUMERO DI TELEFONO: " << c.ntelefono;
+    out << "\nINDIRIZZO: " << c.indirizzo << " " << c.ncivico;
+    return out;
+}
+
+void displaymenu()
+{
+    std::cout << "-------------------------------------\n\n";
+    std::cout << "Benvenuto nella tua agenda personale!\n"
+              << "\nSeleziona una opzione tra le seguenti:\n\n";
+    std::cout << "1. Crea contatto" << "\n2. Modifica contatto" << "\n3. Visualizza contatti" << "\n4. Elimina contatto" << "\n5. Esci";
+}
+
+void getchoice()
+{
+    while (true)
+    {
+        int n;
+        std::cin >> n;
+        switch (n)
+        {
+        case 1:
+            return;
+        case 2:
+            return;
+        case 3:
+            return;
+        case 4:
+            return;
+        case 5:
+            return;
+        default:
+            std::cout << "\nErrore: Opzione non valida.\n";
+        }
+    }
+}
+
+std::vector<Contatto> createContact()
+{ // case 1
     Contatto c;
-    int i = 0;
+    std::vector<Contatto> agenda;
+    std::cout << "\n\n----------------------\n\n";
+    std::cout << "Scrivere 'fine' per terminare creazione contatti.\n";
     while (true)
     {
         std::cin >> c;
-        agenda.push_back(c);
-        char scelta;
-        std::cout << "wanna break from the ads? (y/n): ";
-        std::cin >> scelta;
-        if (scelta == 'y')
+        if (c.nome == "fine" || c.cognome == "fine")
         {
-            break;
+            return agenda;
+        }
+        agenda.push_back(c);
+    }
+}
+
+void modifyContact(Contatto &c)
+{ // case 2
+    std::cout << "\nCosa vuoi modificare?";
+    std::cout << "\n1. Nome" << "\n2. Cognome" << "\n3. Età" << "\n4. Numero di telefono" << "\n5. Indirizzo" << "\n6. Numero civico";
+}
+
+void displayContacts(std::vector<Contatto> &c)
+{ // case 3
+    for (auto &i : c)
+    {
+        std::cout << i;
+    }
+}
+
+void eliminateContact(std::vector<Contatto> &c) // case 4
+{
+    while (true)
+    {
+        std::string info;
+        std::string nome;
+        std::string cognome;
+        bool over = false;
+        std::cout << "\nInserisci il nome e il cognome del contatto che desideri eliminare.\n\n";
+        for (auto &i : c)
+        {
+            std::cout << i.nome << " " << i.cognome << "  |  ";
+        }
+        std::cout << "\n";
+        std::getline(std::cin >> std::ws, info);
+        for (char &i : info)
+        {
+            if (i == ' ')
+            {
+                over = true;
+            }
+            else if (!over)
+            {
+                nome.push_back(i);
+            }
+            else
+            {
+                cognome.push_back(i);
+            }
+        }
+        bool found = false;
+        int index;
+        for (size_t i = 0; i < c.size(); i++)
+        {
+            if (c[i].nome == nome && c[i].cognome == cognome)
+            {
+                c.erase(c.begin() + i);
+                found = true;
+                index = i;
+            }
+        }
+        if (found)
+        {
+            if (c.size() > 0)
+            {
+                for (size_t i = index; i < c.size() - 1; i++)
+                {
+                    c[i] = c[i + 1];
+                }
+            }
+            std::cout << "\nContatto rimosso con successo.\n";
+            return;
+        }
+        else
+        {
+            std::cout << "\nErrore: contatto non esistente.\n";
         }
     }
+}
+
+int main()
+{
+    displaymenu();
+    std::vector<Contatto> c = createContact();
+    displayContacts(c);
+    eliminateContact(c);
+    displayContacts(c);
 }
